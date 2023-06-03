252250998a0328bd68a15fd4fef41482f54b48e6
ayushishr <128039555+ayushishr@users.noreply.github.com>
2023-06-03 13:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function updateOutputText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Call the function and update the HTML element when the promise resolves
+updateOutputText().then((text) => {
+  const outputElement = document.getElementById("output");
+  outputElement.textContent = text;
+});
